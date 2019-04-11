b60826d8479a3c1e035ccbe8d8bdc8c458a09a50
Artem Borisov <dedsa2002@gmail.com>
2019-04-11 17:26:45 +0530

    tissot: Add dalvik property overrides
    * Yes, we already handle this in our init extension, but
      it can't be used with treble.
    
    Change-Id: Ibb31ae9fe5c8d547cd12f329cb694af463eace55

--- prop.mk
+++ prop.mk
@@ -13,3 +13,12 @@ debug.sf.disable_hwc=1
 # Havoc
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.havoc.maintainer=MountaserHalak
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m \
