efc674ce88b15b2338b1cd6707e6bedfdb0a3880
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-12-10 19:29:04 +0530

    tissot: Ship prebuilt Twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,5 +44,11 @@ VENDOR_SECURITY_PATCH := 2019-05-05
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+# Twrp
+-include $(DEVICE_PATH)/twrp/twrp.mk
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk
